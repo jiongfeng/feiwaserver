5dcb48910bc9b15117466f720b47c6cde3bcf9ea
pengxu <1773678198@qq.com>
2017-05-24 13:43:20 +0800

    店铺修改

--- mall/templates/default/home/store_list.php
+++ mall/templates/default/home/store_list.php
@@ -52,120 +52,122 @@ function query(name, value){
 //]]>
 </script>
 
-	   <!--  <div class="feiwa-module feiwa-new-list zoom">
+	     <!-- <div class="feiwa-module feiwa-new-list zoom">
                <div class="pageTab">
-                   <a href="<?php echo urlMall('search','index');?>" class="item1">找商品</a>
-                   <a href="<?php echo urlMall('store_list');?>" class="item2 now">找店铺</a>
-               </div>
-           <?php if(!empty($output['class_list']) && is_array($output['class_list'])){?>    
-          <div class="filter-item area">
-                   <div class="item-cont">
-                       <span class="item-tit feiwa-left">分类</span>
-                       <dl class="feiwa-left item-links">
-                           <dt class="feiwa-left all"><a href="<?php echo urlMall('store_list','index');?>" class="now">不限</a></dt>
-                           <dd class="feiwa-left eachLink leftA">
-                               <div class="clearfix">
-                                         <?php foreach($output['class_list'] as $k=>$v){?>
-         <?php if ($_GET['cate_id'] == $v['sc_parent_id']){?>
-         <span><a href="<?php echo urlMall('store_list','index',array('cate_id'=>$k));?>"><?php echo $v['sc_name'];?></a></span>
-         <?php }elseif (!isset($v['child']) && $output['class_list'][$_GET['cate_id']]['sc_parent_id'] == $v['sc_parent_id']){?>
-         <span><a href="<?php echo urlMall('store_list','index',array('cate_id'=>$k));?>"><?php echo $v['sc_name'];?></a></span>
-         <?php }?>
-             <?php }?>     
-                                                       </div>
-                           </dd>
-                       </dl>
-                   </div>
-               </div>     
-               <?php }?>
+                                <a href="<?php echo urlMall('search','index');?>" class="item1">找商品</a>
+                                <a href="<?php echo urlMall('store_list');?>" class="item2 now">找店铺</a>
+                            </div>
+                        <?php if(!empty($output['class_list']) && is_array($output['class_list'])){?>    
+           <div class="filter-item area">
+                    <div class="item-cont">
+                        <span class="item-tit feiwa-left">分类</span>
+                        <dl class="feiwa-left item-links">
+                            <dt class="feiwa-left all"><a href="<?php echo urlMall('store_list','index');?>" class="now">不限</a></dt>
+                            <dd class="feiwa-left eachLink leftA">
+                                <div class="clearfix">
+                                          <?php foreach($output['class_list'] as $k=>$v){?>
+          <?php if ($_GET['cate_id'] == $v['sc_parent_id']){?>
+          <span><a href="<?php echo urlMall('store_list','index',array('cate_id'=>$k));?>"><?php echo $v['sc_name'];?></a></span>
+          <?php }elseif (!isset($v['child']) && $output['class_list'][$_GET['cate_id']]['sc_parent_id'] == $v['sc_parent_id']){?>
+          <span><a href="<?php echo urlMall('store_list','index',array('cate_id'=>$k));?>"><?php echo $v['sc_name'];?></a></span>
+          <?php }?>
+              <?php }?>     
+                                                        </div>
+                            </dd>
+                        </dl>
+                    </div>
+                </div>     
+                <?php }?> 
+        	
+    <div class="filter-item moreItem">
+ <!--            <div class="item-cont">
+     <span class="item-tit">更多</span>
+           <form id="store_list" method="GET" action="index.php">
+         <input type="hidden" name="order" value="<?php echo $_GET['order'];?>"/>
+         <input type="hidden" name="app" value="store_list"/>
+         <input type="hidden" name="cate_id" value="<?php echo $_GET['cate_id'];?>"/>
+     <?php if (!C('fullindexer.open')){?> <div class="feiwa-left pos-re feiwa-m">
+     <input id="area_info" name="area_info" type="hidden" value=""/>
+ <div class="feiwa-newinput">
+            <input class="text" type="text" name="keyword" value="<?php echo $_GET['keyword'];?>" placeholder="请输入店铺名称"/>
+         </div>
+     </div>
+      <input class="btn" type="submit" value="<?php echo $lang['feiwa_search'];?>" /><?php }?>
+     <div class="clear"></div>
+ </div>
+         </div>
+         </form>-->
+       
+<div class="wrapper  feiwa-new-st">
+        <div class="feiwa-left w965" style="width: 1200px;">
+            <!--排序-->
+            <div class="sort zoom f14">
+                <p class="sort-item">
+                    <a href="javascript:void(0)" onClick="javascript:ss_dropParam('key','order');"  <?php if(!$_GET['key']){?>class="now"<?php }?>>全部店铺</a>
+                    <!-- <a href="">好评</a> -->
+                    <a href="javascript:void(0)" onClick="javascript:ss_replaceParam('store_credit','<?php  echo ($_GET['order'] == 'desc' && $_GET['key'] == 'store_credit')?'asc':'desc' ?>');" <?php if($_GET['key'] == 'store_credit'){?>class="now"<?php }?>>人气</a>
+                    <a href="javascript:void(0)" onClick="javascript:ss_replaceParam('store_sales','<?php echo ($_GET['order'] == 'desc' && $_GET['key'] == 'store_sales')?'asc':'desc' ?>');" <?php if($_GET['key'] == 'store_sales'){?>class="now"<?php }?>>销量</a>
                    
-           <div class="filter-item moreItem">
-                   <div class="item-cont">
-                       <span class="item-tit">更多</span>
-                             <form id="store_list" method="GET" action="index.php">
-               <input type="hidden" name="order" value="<?php echo $_GET['order'];?>"/>
-               <input type="hidden" name="app" value="store_list"/>
-               <input type="hidden" name="cate_id" value="<?php echo $_GET['cate_id'];?>"/>
-                       <?php if (!C('fullindexer.open')){?> <div class="feiwa-left pos-re feiwa-m">
-                       <input id="area_info" name="area_info" type="hidden" value=""/>
-       <div class="feiwa-newinput">
-                              <input class="text" type="text" name="keyword" value="<?php echo $_GET['keyword'];?>" placeholder="请输入店铺名称"/>
-                           </div>
-                       </div>
-                        <input class="btn" type="submit" value="<?php echo $lang['feiwa_search'];?>" /><?php }?>
-                       <div class="clear"></div>
-                   </div>
-               </div>
-               </form>
-               </div>
-       
-       <div class="wrapper  feiwa-new-st">
-               <div class="feiwa-left w965">
-                   排序
-                   <div class="sort zoom f14">
-                       <p class="sort-item">
-                           <a href="javascript:void(0)" onClick="javascript:ss_dropParam('key','order');"  <?php if(!$_GET['key']){?>class="now"<?php }?>>默认</a>
-                           <a href="">好评</a>
-                           <a href="javascript:void(0)" onClick="javascript:ss_replaceParam('store_sales','<?php echo ($_GET['order'] == 'desc' && $_GET['key'] == 'store_sales')?'asc':'desc' ?>');" <?php if($_GET['key'] == 'store_sales'){?>class="now"<?php }?>>销量</a>
-                          <a href="javascript:void(0)" onClick="javascript:ss_replaceParam('store_credit','<?php  echo ($_GET['order'] == 'desc' && $_GET['key'] == 'store_credit')?'asc':'desc' ?>');" <?php if($_GET['key'] == 'store_credit'){?>class="now"<?php }?>>信用</a>
-                       </p>
-                   </div>
-                   //排序
-                   店铺列表
-                   <div class="storesList feiwa-bs">
-       <?php if(!empty($output['store_list']) && is_array($output['store_list'])){?>
-       <?php foreach($output['store_list'] as $skey => $store){?>
-                                       <dl>
-                           <dt class="storesinfo clearfix">
-                               <a target="_blank" href="<?php echo urlMall('show_store','index', array('store_id'=>$store['store_id']),$store['store_domain']);?>" class="storesimg" title="<?php echo $store['store_name'];?>">
-                                   <img src="<?php echo getStoreLogo($store['store_label']);?>" alt="<?php echo $store['store_name'];?>" title="<?php echo $store['store_name'];?>">
-                               </a>
-                               <div class="storesrelated">
-                                   <span class="item1"><a target="_blank" href="<?php echo urlMall('show_store','index', array('store_id'=>$store['store_id']),$store['store_domain']);?>"><?php echo $store['store_name'];?></a></span>
-                                   <div class="item2">
-                                       <div class="inline"><i>资质：</i><b><?php if($store['is_own_mall']==1){;?>本站自营<?php }else{?>第三方店铺<?php }?></b></div>
-                                                                    
-                                                                   </div>
-                                   <span class="item2"><i>地址：</i><b><?php echo $store['store_address'];?></b></span>
-                                   <a target="_blank" href="<?php echo urlMall('show_store','index', array('store_id'=>$store['store_id']),$store['store_domain']);?>" class="item3">查看店铺</a>
-                               </div>
-       
-                                                       <div class="storescase feiwa-right">
-                                   <div class="feiwa-left">
-                                       <span class="levNum"><em><?php echo $store['store_credit_average'];?></em>分</span>
-                                       <p><em class="levbox"><i></i><b style="width:<?php echo $store['store_credit_percent'];?>%"></b></em></p>
-                                       <a href="<?php echo urlMall('show_store','index', array('store_id'=>$store['store_id']),$store['store_domain']);?>" class="c999" target="_blank">已有<?php echo $store['store_collect'];?>人关注</a>
-                                   </div>
-                                   <div class="feiwa-right">
-                                       <?php  foreach ($store['store_credit'] as $key=>$value) {?>
-                                                                           <span><i><?php echo $value['text'];?>：</i><?php echo $value['credit'];?>分</span><?php } ?>
-                                                                   </div>
-                               </div>
-                                                   </dt> 
-                           <?php if(!empty($store['search_list_goods']) && is_array($store['search_list_goods'])){?>
-                                               <dd class="goods-serv stores-serv zoom">
-                               <p>
-                                   <span class="item1">商品名称</span>
-                                   <span class="item5">已收藏</span>
-                                   <span class="item2">本月已销售</span>
-                                   <span class="item3">市场价</span>
-                                   <span class="item4">惊喜价</span>
-                               </p>
-       
-               <?php foreach($store['search_list_goods'] as $k=>$v){?>
-                                                       <a href="<?php echo urlMall('goods','index',array('goods_id'=>$v['goods_id']));?>" target="_blank" title="<?php echo $v['goods_name'];?>">
-                                   <span class="item1"><?php echo $v['goods_name'];?></span>
-                                   <span class="item5"><?php echo $v['goods_collect'];?>人</span>
-                                   <span class="item2"><?php echo $v['goods_salenum'];?>件</span>
-                                   <span class="item3"><del>￥<?php echo $v['goods_marketprice'];?></del></span>
-                                   <span class="item4"><em>￥<?php echo $v['goods_promotion_price'];?></em></span>
-                               </a> <?php }?>
-       <div class="checkMore"><a target="_blank" href="<?php echo urlMall('show_store','index', array('store_id'=>$store['store_id']),$store['store_domain']);?>">更多<?php echo ($tmp = $store['goods_count']) ? $tmp  : $lang['feiwa_common_goods_null'];?>款商品，买吗&gt;&gt;</a></div>
-                                                   </dd><?php }?>
-                                           </dl><?php }?>
-       <?php }?>
+                </p>
+            </div>
+            <!--//排序-->
+            <!--店铺列表-->
+            <div class="storesList feiwa-bs">
+<?php if(!empty($output['store_list']) && is_array($output['store_list'])){?>
+<?php foreach($output['store_list'] as $skey => $store){?>
+                                <dl>
+                    <dt class="storesinfo clearfix">
+                        <a target="_blank" href="<?php echo urlMall('show_store','index', array('store_id'=>$store['store_id']),$store['store_domain']);?>" class="storesimg" title="<?php echo $store['store_name'];?>">
+                            <img src="<?php echo getStoreLogo($store['store_label']);?>" alt="<?php echo $store['store_name'];?>" title="<?php echo $store['store_name'];?>">
+                        </a>
+                        <div class="storesrelated">
+                            <span class="item1"><a target="_blank" href="<?php echo urlMall('show_store','index', array('store_id'=>$store['store_id']),$store['store_domain']);?>"><?php echo $store['store_name'];?></a></span>
+                            <div class="item2">
+                                <div class="inline"><i>资质：</i><b><?php if($store['is_own_mall']==1){;?>本站自营<?php }else{?>第三方店铺<?php }?></b></div>
+                                                             
+                                                            </div>
+                            <span class="item2"><i>地址：</i><b><?php echo $store['store_address'];?></b></span>
+                            <!-- <a target="_blank" href="<?php echo urlMall('show_store','index', array('store_id'=>$store['store_id']),$store['store_domain']);?>" class="item3">查看店铺</a> -->
+                            <div>
+                                <?php  foreach ($store['store_credit'] as $key=>$value) {?>
+                                <span><i><?php echo $value['text'];?>：</i><?php echo $value['credit'];?>分</span><?php } ?>
+                            </div>
+                        </div>
+
+                                                <div class="storescase feiwa-right">
+                            <!-- <div class="feiwa-left">
+                                <span class="levNum"><em><?php echo $store['store_credit_average'];?></em>分</span>
+                                <p><em class="levbox"><i></i><b style="width:<?php echo $store['store_credit_percent'];?>%"></b></em></p>
+                                <a href="<?php echo urlMall('show_store','index', array('store_id'=>$store['store_id']),$store['store_domain']);?>" class="c999" target="_blank">已有<?php echo $store['store_collect'];?>人关注</a>
+                            </div> -->
                             
-                                   </div> -->
+                        </div>
+                                            </dt> 
+                    <?php if(!empty($store['search_list_goods']) && is_array($store['search_list_goods'])){?>
+                                        <dd class="goods-serv stores-serv zoom">
+                      <!--   <p>
+                          <span class="item1">商品名称</span>
+                          <span class="item5">已收藏</span>
+                          <span class="item2">本月已销售</span>
+                          <span class="item3">市场价</span>
+                          <span class="item4">惊喜价</span>
+                      </p> -->
+
+        <?php foreach($store['search_list_goods'] as $k=>$v){?>
+                                                <a href="<?php echo urlMall('goods','index',array('goods_id'=>$v['goods_id']));?>" target="_blank" title="<?php echo $v['goods_name'];?>">
+                            <div style="width: 120px;height: 120px;margin:5px auto;"><img src="gjhfgfj/xfcv/cxv" style="width: 120px;height: 120px;" /></div>
+                            <p class="item1"><?php echo $v['goods_name'];?></p>
+                            <!-- <span class="item5"><?php echo $v['goods_collect'];?>人</span>
+                            <span class="item2"><?php echo $v['goods_salenum'];?>件</span> -->
+                            <!-- <span class="item3"><del>￥<?php echo $v['goods_marketprice'];?></del></span> 原价-->
+                            <span class="item4"><em>￥<?php echo $v['goods_promotion_price'];?></em></span>
+                        </a> <?php }?>
+<!-- <div class="checkMore"><a target="_blank" href="<?php echo urlMall('show_store','index', array('store_id'=>$store['store_id']),$store['store_domain']);?>">更多<?php echo ($tmp = $store['goods_count']) ? $tmp  : $lang['feiwa_common_goods_null'];?>款商品，买吗&gt;&gt;</a></div>-->
+                                            </dd><?php }?>
+                                    </dl><?php }?>
+<?php }?>
+                     
+                            </div> 
                             
             <!--//店铺列表-->
  <!--翻页-->
@@ -173,11 +175,11 @@ function query(name, value){
             <!--//翻页-->
         </div>
 
-        <div class="feiwa-right w220">
-            <!-- <div class="mb15">
+        <!-- <div class="feiwa-right w220">
+            <div class="mb15">
                 <a href="http://www.feiwa.org" target="_blank"><img src="<?php echo MALL_SITE_URL;?>/img/whyYw.jpg" alt="为什么选择FeiWa" class="mb15"></a>
-            </div > -->
-            <!--销量排行-->
+            </div >
+            销量排行
             <?php if(!empty($output['goods_list']) && is_array($output['goods_list'])){$i=0?>
             <div class="w218 sales feiwa-bs">
                 <p class="sideTit">销量排行</p>
@@ -192,15 +194,15 @@ function query(name, value){
         
                             </div><?php }?>
            
-            <!-- <div class="for-fix">
+            <div class="for-fix">
                 <div class="mb15">
                     <a href="http://www.demo.feiwa.org" target="_blank"><img src="<?php echo MALL_SITE_URL;?>/img/taoListApp.jpg" alt="FeiWaS5"></a>
                 </div>
                 <div class="mb15">
                     <a href="http://www.feiwa.org" target="_blank"><img src="<?php echo MALL_SITE_URL;?>/img/pei.jpg" alt="FeiWa代运营"></a>
                 </div>
-            </div> -->
-        </div>
+            </div>
+        </div> -->
     </div>
 </div>
 <script type="text/javascript" src="<?php echo RESOURCE_SITE_URL;?>/js/waypoints.js"></script> 
@@ -220,6 +222,12 @@ $(function(){
     		$('div[feiwa_type="goods_all"]').hide();
     	}
     });
+
+    $(".sortbar-array ul li").click(function(){
+        $(".sortbar-array ul li").removeClass();
+        $(this).addClass("selected");
+
+    })
    
 });
 </script>
