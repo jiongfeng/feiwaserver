d09bb97d29bb596b3755ef3593f1a59c85395846
jiongfeng_OFFICE_PC <914767350@qq.com>
2017-07-21 17:18:04 +0800

    测试邮件

--- admin/modules/system/control/message.php
+++ admin/modules/system/control/message.php
@@ -140,6 +140,8 @@ class messageControl extends SystemControl{
 			$obj_email->set('email_from',$email_addr);
             $obj_email->set('site_name',$site_title);
 			$result = $obj_email->send($email_test,$subject,$message);
+			dump($result);
+			exit;
 // 		}else {
 // 			$result = @mail($email_test,$subject,$message);
 // 		}
